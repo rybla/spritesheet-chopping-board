b9e84c43f7c6b8858fb2991af7d9dc413d10dedb
Henry Blanchette <henry@rybl.net>
2026-07-07 17:25:43 -0400

    deps

--- src/pages/Home.page.tsx
+++ src/pages/Home.page.tsx
@@ -1,11 +1,12 @@
 import { ColorSchemeToggle } from "@/components/ColorSchemeToggle/ColorSchemeToggle";
 import { Welcome } from "@/components/Welcome/Welcome";
+import classes from "@/pages/Home.module.css";
 
 export function HomePage() {
   return (
-    <>
+    <div className={classes["page"]}>
       <Welcome />
       <ColorSchemeToggle />
-    </>
+    </div>
   );
 }
